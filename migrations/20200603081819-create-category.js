3ed9b7e6a4f0ee70bcbc07c4d2ac066ec78294d7
Basith Kunimal <basith@intellicar.in>
2020-06-24 01:59:29 +0530

    category images

--- migrations/20200603081819-create-category.js
+++ migrations/20200603081819-create-category.js
@@ -12,6 +12,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING
       },
+      ImageId: {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- models/category.js
+++ models/category.js
@@ -2,10 +2,12 @@
 module.exports = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
     name: DataTypes.STRING,
+    ImageId: DataTypes.INTEGER,
   }, {});
   Category.associate = function(models) {
     // associations can be defined here
     Category.hasMany(models.Product, { onDelete: 'RESTRICT' });
+    Category.belongsTo(models.Image);
   };
   return Category;
 };
